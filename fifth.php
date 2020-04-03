a3904bd74270f230ccc5e5acc5f16f284e84071f
runacode <59107019+runacode@users.noreply.github.com>
2020-04-03 12:57:41 -0500

    Initial commit

---
+++ fifth.php
@@ -0,0 +1,4 @@
+<?php
+    $overwrite = "fifth_php.json";
+     include_once($_SERVER['DOCUMENT_ROOT'] . '/PageRenderer.php');
+    ?>
---
+++ fourth.php
@@ -0,0 +1,4 @@
+<?php
+    $overwrite = "fourth_php.json";
+     include_once($_SERVER['DOCUMENT_ROOT'] . '/PageRenderer.php');
+    ?>
